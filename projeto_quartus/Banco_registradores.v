2837ceff6d981085594dc3254dd4357889fc60e2
Renato Júnior <renatosergiolopez@hotmail.com>
2017-10-06 20:55:32 -0300

    Coloca banco de registradores

---
+++ projeto_quartus/Banco_registradores.v
@@ -0,0 +1,23 @@
+module Banco_registradores(regA, regB, dado, regC, RW, clk, regsaidaA, regsaidaB);
+input [5:0] regA;
+input [5:0] regB;
+input [15:0] dado;
+input [5:0] regC;
+input RW; //0 - leitura / 1 - escrita
+input clk;
+output reg [15:0] regsaidaA;
+output reg [15:0] regsaidaB;
+
+reg [15:0] registradores [31:0];
+
+always @(posedge clk)
+begin
+	if(RW == 0) begin
+		regsaidaA <= registradores[regA][15:0];
+		regsaidaB <= registradores[regB][15:0];
+	end else begin
+		registradores[regC] <= dado;
+	end
+end
+
+endmodule
