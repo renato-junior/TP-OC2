cdc89a7e5d362669d3b178c111d702a81c6c4391
Renato Júnior <renatosergiolopez@hotmail.com>
2017-10-10 22:07:44 -0300

    Adiciona mais controle

--- projeto_quartus/Processador.v
+++ projeto_quartus/Processador.v
@@ -2,37 +2,60 @@ module Processador(CLOCK_50, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, 
 input CLOCK_50;
 input [3:0] KEY;
 input [17:0] SW;
-output reg [6:0] HEX0;
-output reg [6:0] HEX1;
-output reg [6:0] HEX2;
-output reg [6:0] HEX3;
-output reg [6:0] HEX4;
-output reg [6:0] HEX5;
-output reg [6:0] HEX6;
-output reg [6:0] HEX7;
+output [6:0] HEX0;
+output [6:0] HEX1;
+output [6:0] HEX2;
+output [6:0] HEX3;
+output [6:0] HEX4;
+output [6:0] HEX5;
+output [6:0] HEX6;
+output [6:0] HEX7;
 
-reg [16:0] saidaA;
-reg [16:0] saidaB;
+//Componentes do Banco de Registradores
+reg bancoRW;
+reg [4:0] endRegA;
+reg [4:0] endRegB;
+reg [4:0] endRegC;
+reg [15:0] dadoBanco;
+wire [15:0] saidaA;
+wire [15:0] saidaB;
 
 Banco_registradores banco(
-	.regA(SW[11:8]),
-	.regB(SW[7:4]),
-	.RW(0),
+	.regA(endRegA),
+	.regB(endRegB),
+	.regC(endRegC),
+	.RW(bancoRW),
+	.dado(dadoBanco),
 	.clk(CLOCK_50),
 	.regsaidaA(saidaA),
 	.regsaidaB(saidaB)
 );
-conversor7segmentos conversor1(
+
+//Componentes da ALU
+reg [4:0] codop;
+reg [15:0] imm;
+wire [15:0] resultadoALU;
+
+ALU alu(
+	.clk(CLOCK_50),
+	.codop(codop),
+	.operando1(saidaA),
+	.operando2(saidaB),
+	.resultado(resultadoALU)
+);
+
+
+conversor7segmentos conversor7(
 	.clk(CLOCK_50),
 	.dado(saidaA[7:4]),
 	.z(HEX7)
 );
-conversor7segmentos conversor2(
+conversor7segmentos conversor6(
 	.clk(CLOCK_50),
 	.dado(saidaA[3:0]),
 	.z(HEX6)
 );
-conversor7segmentos conversor3(
+conversor7segmentos conversor5(
 	.clk(CLOCK_50),
 	.dado(saidaB[7:4]),
 	.z(HEX5)
@@ -40,13 +63,93 @@ conversor7segmentos conversor3(
 conversor7segmentos conversor4(
 	.clk(CLOCK_50),
 	.dado(saidaB[3:0]),
-	.z(HEX6)
+	.z(HEX4)
+);
+conversor7segmentos conversor3(
+	.clk(CLOCK_50),
+	.dado(resultadoALU[15:12]),
+	.z(HEX3)
+);
+conversor7segmentos conversor2(
+	.clk(CLOCK_50),
+	.dado(resultadoALU[11:8]),
+	.z(HEX2)
+);
+conversor7segmentos conversor1(
+	.clk(CLOCK_50),
+	.dado(resultadoALU[7:4]),
+	.z(HEX1)
+);
+conversor7segmentos conversor0(
+	.clk(CLOCK_50),
+	.dado(resultadoALU[3:0]),
+	.z(HEX0)
 );
 
 always @(posedge CLOCK_50)
 begin
 	if(KEY[0] == 1) begin
+		endRegA = SW[11:8];
+		endRegB = SW[7:4];
+		bancoRW = 0;
+	end
+	if(KEY[3] == 1) begin
+		bancoRW = 0;
+		codop = SW[15:12];
+		if(codop == 4'd0) begin
+			endRegC = SW[11:8];
+			endRegA = SW[7:4];
+			endRegB = SW[3:0];
+		end else if(codop == 4'd1) begin
+			endRegC = SW[11:8];
+			endRegA = SW[7:4];
+			endRegB = SW[3:0];
+		end else if(codop == 4'd2) begin
+			endRegC = SW[11:8];
+			endRegA = SW[7:4];
+			endRegB = SW[3:0];
+		end else if(codop == 4'd3) begin
+			endRegC = SW[11:8];
+			endRegA = SW[7:4];
+			endRegB = SW[3:0];
+		end else if(codop == 4'd4) begin
+			endRegC = SW[11:8];
+			endRegA = SW[7:4];
+			endRegB = SW[3:0];
+		end else if(codop == 4'd5) begin
+			endRegC = SW[11:8];
+			endRegA = SW[7:4];
+			endRegB = SW[3:0];
+		end else if(codop == 4'd6) begin
+			endRegC = SW[11:8];
+			imm[15:4] = 12'd0;
+			imm[3:0] = SW[7:4];
+			endRegB = SW[3:0];
+		end
+//		end else if(codop == 4'd7) begin
+//			endRegC = SW[11:8];
+//			saidaA[15:4] = 12'd0;
+//			saidaA[3:0] = SW[7:4];
+//			endRegB = SW[3:0];
+//		end else if(codop == 4'd8) begin
+//			endRegC = SW[11:8];
+//			saidaA[15:4] = 12'd0;
+//			saidaA[3:0] = SW[7:4];
+//			endRegB = SW[3:0];
+//		end else if(codop == 4'd9) begin
+//			endRegC = SW[11:8];
+//			saidaA[15:4] = 12'd0;
+//			saidaA[3:0] = SW[7:4];
+//			endRegB = SW[3:0];
+//		end else if(codop == 4'd10) begin
+//			endRegC = SW[11:8];
+//			saidaA[15:4] = 12'd0;
+//			saidaA[3:0] = SW[7:4];
+//			endRegB = SW[3:0];
+//		end
 		
+		dadoBanco = resultadoALU;
+		bancoRW = 1;
 	end
 
 
