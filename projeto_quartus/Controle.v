4eaf3bdd4fa05062dba079fa2f36e59573f07c10
viniciusvecchia <viniciusvecchia@gmail.com>
2017-11-03 19:37:52 -0200

    Correção na logica de controle

--- projeto_quartus/Controle.v
+++ projeto_quartus/Controle.v
@@ -1,19 +1,22 @@
 module Controle(clk, opcode, EscCondCP, EscCP, ULA_OP, ULA_A, ULA_B, EscIR, FonteCP, EscReg);
 input clk;
 input [3:0] opcode;
-output reg EscCondCP, EscCP, [3:0] ULA_OP, ULA_A, [1:0] ULA_B, EscIR, FonteCP, EscReg;
+output reg EscCondCP, EscCP, ULA_OP, ULA_A, ULA_B, EscIR, FonteCP, EscReg;
 
 always @(posedge clk)
 begin
 	ULA_OP = opcode;
-	if (opcode == 4b'1011 || opcode == 4b'1100) 		//Gera sinal EscCondCP
-		4b'1011: EscCondCP = 1;
-	else EscCondCP = 0;
+	if ( opcode[3:0] == 4'b1011 || opcode [3:0] == 4'b1100 ) 		//Gera sinal EscCondCP
+			EscCondCP = 1;
+	else 
+			EscCondCP = 0;
 		
-	if (opcode == 4d'11) 								//Se opcode==jump ULA_B=2b'10;
-		ULA_B = 2b'10;
+		
+	if (opcode == 4'd11)							//Se opcode==jump ULA_B=2b'10;
+		ULA_B = 2'b10;
 	else
-		ULA_B = 2b'00;
+		ULA_B = 2'b00;
+	
 		
 end
 
