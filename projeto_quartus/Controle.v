55e24d8c3aa867099aa0c09160b5880249544162
Renato Júnior <renatosergiolopez@hotmail.com>
2017-11-03 09:50:47 -0200

    Coloca os inputs e outputs do controle

--- projeto_quartus/Controle.v
+++ projeto_quartus/Controle.v
@@ -1,1 +1,12 @@
+module Controle(clk, opcode, EscCondCP, EscCP, ULA_OP, ULA_A, ULA_B, EscIR, FonteCP, EscReg);
+input clk;
+input [3:0] opcode;
+output reg EscCondCP, EscCP, ULA_OP, ULA_A, ULA_B, EscIR, FonteCP, EscReg;
+
+always @(posedge clk)
+begin
+
+end
+
+endmodule
  
