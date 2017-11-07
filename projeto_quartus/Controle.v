788e4399c7a31d4f739017d22e52463e4237bb8e
Renato Júnior <renatosergiolopez@hotmail.com>
2017-11-07 20:42:47 -0200

    Coloca inteiro de estado no controle

--- projeto_quartus/Controle.v
+++ projeto_quartus/Controle.v
@@ -7,7 +7,7 @@ output reg [1:0]ULA_B;
 output reg [1:0]FonteCP;
 
 
-reg [1:0] estado;
+integer estado;
 
 initial begin
 	estado = 2'd0;
@@ -16,57 +16,53 @@ end
 always @(posedge clk)
 begin
 	if(opcode != ULA_OP) begin
-		estado = 2'd0;
+		estado = 0;
 	end
 	ULA_OP = opcode;
 	
 	if (opcode == 4'd0 || opcode == 4'd1 || opcode == 4'd3 || opcode == 4'd4 || opcode == 4'd5)
 		begin
-			if(estado == 2'd0) begin
+			if(estado == 0) begin
 				EscCondCP = 0;
-				EscCP = 1;
+				EscCP = 0;
 				ULA_A = 1;
 				ULA_B = 00;
 				EscIR = 0;
 				FonteCP = 00;
 				EscReg = 0;
-				estado = 2'd1;
-			end else if(estado == 2'd1) begin 
-				EscCondCP = 0;
-				EscCP = 1;
-				ULA_A = 1;
-				ULA_B = 00;
-				EscIR = 0;
-				FonteCP = 00;
-				EscReg = 0;
-				estado = 2'd2;
-			end else if(estado == 2'd2) begin 
+				estado = estado + 1;
+			end else if(estado == 4) begin 
 				EscReg = 1;
-			end
+				estado = estado + 1;
+			end else if(estado == 6) begin 
+				EscCP = 1;
+				EscReg = 0;
+				estado = 0;
+			end else begin
+				estado = estado + 1;
+			end	
 		end
 		
 	if (opcode == 4'd2 || opcode == 4'd6 || opcode == 4'd7 || opcode == 4'd8 || opcode == 4'd9 || opcode == 4'd10)	
 		begin
-			if(estado == 2'd0) begin
+			if(estado == 0) begin
 				EscCondCP = 0;
-				EscCP = 1;
+				EscCP = 0;
 				ULA_A = 1;
 				ULA_B = 00;   //operador imediato
 				EscIR = 0;
 				FonteCP = 00;
 				EscReg = 0;
-				estado = 2'd1;
-			end else if(estado == 2'd1) begin 
-				EscCondCP = 0;
-				EscCP = 1;
-				ULA_A = 1;
-				ULA_B = 00;   //operador imediato
-				EscIR = 0;
-				FonteCP = 00;
-				EscReg = 0;
-				estado = 2'd2;
-			end else if(estado == 2'd2) begin 
+				estado = estado + 1;
+			end else if(estado == 4) begin 
 				EscReg = 1;
+				estado = estado + 1;
+			end else if(estado == 6) begin 
+				EscCP = 1;
+				EscReg = 0;
+				estado = 0;
+			end else begin
+				estado = estado + 1;
 			end
 		end
 		
